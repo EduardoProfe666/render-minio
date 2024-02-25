7a13732868e67a9fa14509161581c8819c08eea1
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2024-02-25 13:28:33 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,1 @@
-#!/usr/bin/env sh
-# Script de entrada para configurar MinIO
 
-# Ejecuta el comando de entrada original de MinIO con la dirección de la consola y el puerto expuesto
-exec docker-entrypoint.sh server --console-address "':9002'" --address "':9000'" /data
